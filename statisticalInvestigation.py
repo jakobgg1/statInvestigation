38ba37fb988914202134bfdfce6b5ca38b5b6b2d
Lucas Häyhänen <lucas.hayhanen@live.se>
2024-10-15 14:13:05 +0200

    Co-authored-by: jakobgg1 <jakobgg1@users.noreply.github.com> Co-authored-by: GustafBergh <GustafBergh@users.noreply.github.com>

--- statisticalInvestigation.py
+++ statisticalInvestigation.py
@@ -1,5 +1,11 @@
 import matplotlib.pyplot as plt
 import math
+import numpy as np
+from scipy.linalg import lstsq
+
+x = [2018, 2019, 2019, 2020, 2020, 2023]
+y = [94_000_000, 1_500_000_000, 8_300_000_000, 17_200_000_000, 175_000_000_000, 1_800_000_000_000]
+#---------------------1 ----------------------------
 def data():
     elmo = [2018, 94_000_000]
     gpt2 = [2019, 1_500_000_000]
@@ -9,11 +15,15 @@ def data():
     gpt4 = [2023, 1_800_000_000_000]
     
     models = [elmo, gpt2, megatron_lm, turing, gpt3, gpt4]
-    
-#https://medium.com/@haytamborquane/nvidia-megatron-lm-model-ae9c91794eec
-#for gpt4: https://explodingtopics.com/blog/gpt-parameters
     return models
 
+# 1a   
+#   
+#   https://medium.com/@haytamborquane/nvidia-megatron-lm-model-ae9c91794eec
+#   for gpt4: https://explodingtopics.com/blog/gpt-parameters
+    
+
+2
 def plot(data):
     years = []
     parameters = []
@@ -23,7 +33,7 @@ def plot(data):
         parameters.append(e[1])
         log_parameters.append(math.log10(e[1]))
 
-    a, b = regression_estimation(log_parameters) #excercise 2
+    alfa, beta = linearLeastSqueres(years, log_parameters) #excercise 2
 
 
     fig, ax1 = plt.subplots()
@@ -33,28 +43,38 @@ def plot(data):
     plt.show(block=False)
 
     fig2, ax2 = plt.subplots()
+    
 
     ax2.scatter(years, log_parameters, c='red') #c-d)
+    
+    ax2.plot(x, float(alfa)*x + float(beta), 'r', label='Fitted line')
     ax2.set_title('logaritm')
     plt.show()
+    
+#answers 1e):
+#seems to be growing exponantially, not linearly
+#---------------------2 ----------------------------
+#2a 
+#   a = initial, b = growth rate, x year, c deviations.
+#   Morse law - historically nr of transistors per computer chip doubles every two years. (https://ourworldindata.org/moores-law)
+    
+#2b&c
+def linearLeastSqueres(x, y):
+    x = np.array(x)
+    y = np.array(y)
+    A = np.vstack([x, np.ones(len(x))]).T
+    a, b = np.linalg.lstsq(A, y)[0]
 
-def regression_estimation(y):
+    _ = plt.plot(x, y, 'o', label='Original data', markersize=10)
+    _ = plt.plot(x, a*x + b, 'r', label='Fitted line')
+    _ = plt.legend()
+    plt.show()
 
-    pass
+#2c
 
 
 
 def main():
     plot(data())
-
-
+    #linearLeastSqueres(x, y)
 main()
-
-#answers 1e):
-#seems to be growing exponantially, not linear
-
-
-#---------------------2 ----------------------------
-#a = initial, b = growth rate, x year, c deviations.
-#https://ourworldindata.org/moores-law 
-#historically nr of transistors per computer chip has doubled.
