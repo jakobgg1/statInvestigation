0dd85685d737f021c97308ee19d5f55ecc08ab4c
jakobgg1 <147390149+jakobgg1@users.noreply.github.com>
2024-10-16 15:33:09 +0200

    3klar

--- statisticalInvestigation.py
+++ statisticalInvestigation.py
@@ -23,7 +23,7 @@ def data():
 #   for gpt4: https://explodingtopics.com/blog/gpt-parameters
     
 
-2
+
 def plot(data):
     years = []
     parameters = []
@@ -43,27 +43,26 @@ def plot(data):
         print(data[i][0], '=', diff, 'expected linearvalue:', temp) #2d)
     '''
     years = np.array(years)
-    regression_line = (beta) * years + (alfa)
+    regression_line = beta * years + alfa
     regression_line_nonlog = beta_nonlog * years + alfa_nonlog 
-    #parameters = np.array(parameters)
 
-    
     fig, ax1 = plt.subplots()
-
     ax1.scatter(years, parameters, c='blue') #b)
     ax1.set_title('Non-logarithmic')
     ax1.plot(years, regression_line_nonlog, c='red')
     plt.show(block=False)
 
-
     fig2, ax2 = plt.subplots()
     ax2.scatter(years, log_parameters, c='red') #c-d)
     ax2.plot(years, regression_line, 'r', label='Fitted line') #2e)
-
     ax2.set_title('logaritm')
     plt.show()
 
-    #residual line:
+
+    #2f)
+    #by reading graph from x,y = 2018, 9  -> 2019.4, 10
+    # we see a factor 10 increase which gives 1.4 years
+    
   
 #answers 1e):
 #seems to be growing exponantially, not linearly
@@ -86,7 +85,55 @@ def linearLeastSqueres(x, y):
 
 def main():
     plot(data())
-    #linearLeastSqueres(x, y)
 
 if __name__ == "__main__":
     main()
+
+#----------------- 3a -----------------------------
+
+# Använder formeln för T-testet vilket ger t = 0.8/(σ (0.8)) ≈ 0.8/0.414 = 1.93
+
+# degrees of freedom, 6-2 = 4 : α = 0.05
+
+# Jämför vårt värde med t-tabell och får 2.776, eftersom vårat värde är mindre kan inte en linjär
+
+# signifikant relation bevisas.
+
+ 
+
+#----------------- 3b -----------------------------
+
+# konfidensintervall ges av 0.8 +- 2.776 * 0.414 vilket ger (-0.35,1.95)
+
+# Något som visar på att lutningen kan variera mellan -0.35 och 1.95. Eftersom
+
+# lutningen innefattar 0, kan inte med säkerhet säga att lutningen är positiv. Detta
+
+# medför en osökerhet att svara på frågan med detta konfidensintervall.
+
+ 
+
+ 
+
+#----------------- 3c -----------------------------
+
+# För 2025 kan vi förvänta oss:
+
+# y = 0.8 * 2025 - 1604.56 = 6.44 | För att beräkna konfidensintervall för 2025:
+
+# konfidensintervall = 6.44 +- 2.776 * 1.593 = (1.00,11.88)
+
+# konfidensintervall är så pass stort vilket medför en stor osäkerhet
+
+ 
+
+#----------------- 3d -----------------------------
+
+# 0.80*2030−1604.56=8.44
+
+# konfidensintervall 8.44 +- 2.776 * .593 = (3.00,13.88)
+
+#
+
+# Slutsats likt 3c
+
